d1566495d68d0c46c59c1c793dfa69d89c2c3824
Op-Pautu <pautuop@gmail.com>
2023-08-08 23:15:08 +0530

    Add and style products and single product page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,43 @@
 "use client"
 
 import CarouselComp from './components/CarouselComp'
+import Product from './components/Product'
 import MainLayout from './layouts/MainLayout'
 export default function Home() {
+  
+  const products = [
+    {
+      id: 1,
+      title: 'Brown Leather Bag',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsum porro optio. Quidem omnis sunt autem. Sint, ipsa doloribus ullam necessitatibus perferendis, quia porro eum possimus, similique assumenda corporis laudantium!",
+      url: 'https://picsum.photos/id/7',
+      price: 2500
+    },
+    {
+      id: 2,
+      title: 'School Books',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsum porro optio. Quidem omnis sunt autem. Sint, ipsa doloribus ullam necessitatibus perferendis, quia porro eum possimus, similique assumenda corporis laudantium!",
+      url: 'https://picsum.photos/id/20',
+      price: 1999
+    }
+  ]
+  
+  
   return (
     <main className="">
-        <MainLayout>
-          <CarouselComp/>
-        </MainLayout>
-    </main>
-  )
-}
+    <MainLayout>
+    <CarouselComp/>
+    <div className='max-w-[1200px] mx-auto'>
+    <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+    <div className="grid grid-cols-5 gap-4">
+    { products.map(product => (
+      <Product key={product.id} product={product}/>
+      ))}
+      </div>
+      
+      </div>
+      </MainLayout>
+      </main>
+      )
+    }
+    

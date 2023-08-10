9352a51c2e03d0c87cdbdd01ffec06752e05dc94
Op-Pautu <pautuop@gmail.com>
2023-08-10 15:29:20 +0530

    Add auth and address pages

---
+++ app/address/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import TextInput from '../components/TextInput'
+
+export default function Address() {
+  return (
+    <>
+        <MainLayout>
+            <div id="AddressPage" className='mt-4 max-w-[600px] mx-auto px-2'>
+                <div className="mx-auto bg-white rounded-lg p-3">
+                    <div className="text-xl text-bold mb-2">Address Details</div>
+                    <form>
+                        <div className="mb-4">
+                            <TextInput 
+                                className="w-full"
+                                string={'TEST'}
+                                placeholder="Name"
+                               
+                            />
+                        </div>
+                        <button className='mt-6 w-full text-white text-lg font-semibold p-3 rounded bg-blue-600'>Update Address</button>
+                    </form>
+                </div>    
+            </div>    
+        </MainLayout> 
+    </>
+  )
+}

777a978f37441d2b1cae5ef68489955f27b9e12d
Op-Pautu <pautuop@gmail.com>
2023-08-15 22:14:02 +0530

    Finish main page and similar products page

--- app/components/SimilarProducts.jsx
+++ app/components/SimilarProducts.jsx
@@ -1,50 +1,53 @@
 "use client"
-
+import { useEffect, useState } from "react"
 import { BiLoader } from "react-icons/bi"
 import Product from "./Product"
 
 const SimilarProducts = () => {
-    const products = [
-        {
-            id: 1,
-            title: 'Brown Leather Bag',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsum porro optio. Quidem omnis sunt autem. Sint, ipsa doloribus ullam necessitatibus perferendis, quia porro eum possimus, similique assumenda corporis laudantium!",
-            url: 'https://picsum.photos/id/7',
-            price: 2500
-        },
-        {
-            id: 2,
-            title: 'School Books',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsum porro optio. Quidem omnis sunt autem. Sint, ipsa doloribus ullam necessitatibus perferendis, quia porro eum possimus, similique assumenda corporis laudantium!",
-            url: 'https://picsum.photos/id/20',
-            price: 1999
+    const [products, setProducts] = useState([])
+
+    const getRandomProducts = async () => {
+        try {
+          const response = await fetch('/api/products/get-random')
+          const result = await response.json()
+    
+          if (result) {
+            setProducts(result)
+            return
+          }
+    
+          setProducts([])
+        } catch (error) {
+          console.log(error)
+          alert(error)
         }
-    ]
+      }
+    
+      useEffect(() => { getRandomProducts() }, [])
     
     return (
         <>
-            <div>
-                <div className="border-b py-1 mx-auto max-w-[1200px]"/>
+        <div>
+            <div className="border-b py-1 max-w-[1200px] mx-auto" />
 
-                <div className="max-w-[1200px] mx-auto">
-                    <div className="font-bold text-2xl py-2 mt-4">Similar sponspored items</div>
-                        
-                        {products.length > 0 ? 
-                            <div className="grid grid-cols-5 gap-4">
-                                {products.map(product => (
-                                    <Product key={product.id} product={product}/>
-                                ))}
-                            </div>
-                          : <div className="flex items-center justify-center">
-                                <div className="flex items-center justify-center gap-4 font-semibold">
-                                    <BiLoader size={30} className="text-blue-400 animate-spin"/>
-                                    Loading products...
-                                </div>
-                            </div>
-                        }
-                </div>
+            <div className="max-w-[1200px] mx-auto">
+                <div className="font-bold text-2xl py-2 mt-4">Similar sponsored items</div>
+
+                {products.length > 0 ?
+                  <div className="grid grid-cols-5 gap-4">
+                    {products.map(product => (
+                        <ProductComp key={product.id} product={product} />
+                    ))}
+                  </div>
+                : <div className="flex items-center justify-center">
+                      <div className="flex items-center justify-center gap-4 font-semibold">
+                          <BiLoader size={30} className="text-blue-400 animate-spin"/>
+                          Loading Products...
+                      </div>
+                  </div>}
             </div>
-        </>
+        </div>
+    </>
             )
         }
         
--- app/page.js
+++ app/page.js
@@ -1,43 +1,43 @@
-"use client"
+"use client";
 
+import { useEffect, useState } from 'react';
 import CarouselComp from './components/CarouselComp'
-import Product from './components/Product'
-import MainLayout from './layouts/MainLayout'
+import Product from './components/Product';
+import MainLayout from './layouts/MainLayout';
+import useIsLoading from "@/app/hooks/useIsLoading"
+
 export default function Home() {
-  
-  const products = [
-    {
-      id: 1,
-      title: 'Brown Leather Bag',
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsum porro optio. Quidem omnis sunt autem. Sint, ipsa doloribus ullam necessitatibus perferendis, quia porro eum possimus, similique assumenda corporis laudantium!",
-      url: 'https://picsum.photos/id/7',
-      price: 2500
-    },
-    {
-      id: 2,
-      title: 'School Books',
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsum porro optio. Quidem omnis sunt autem. Sint, ipsa doloribus ullam necessitatibus perferendis, quia porro eum possimus, similique assumenda corporis laudantium!",
-      url: 'https://picsum.photos/id/20',
-      price: 1999
-    }
-  ]
-  
-  
+
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    useIsLoading(true)
+
+    const response = await fetch('/api/products')
+    const prods = await response.json()
+
+    setProducts([])
+    setProducts(prods)
+    useIsLoading(false)
+  }
+
+  useEffect(() => { getProducts() }, [])
+
   return (
-    <main className="">
-    <MainLayout>
-    <CarouselComp/>
-    <div className='max-w-[1200px] mx-auto'>
-    <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
-    <div className="grid grid-cols-5 gap-4">
-    { products.map(product => (
-      <Product key={product.id} product={product}/>
-      ))}
-      </div>
-      
-      </div>
-      </MainLayout>
-      </main>
-      )
-    }
-    
+    <>
+        <MainLayout>
+          <CarouselComp />
+
+          <div className="max-w-[1200px] mx-auto">
+            <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+
+            <div className="grid grid-cols-5 gap-4">
+              {products.map(product => (
+                <Product key={product.id} product={product} />
+              ))}
+            </div>
+          </div>
+        </MainLayout>
+    </>
+  )
+}
